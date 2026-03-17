7a866ad0bcc5cde1a9b6e0eab555f2a73b599af3
maxonchickdev <timetotime828@gmail.com>
2026-03-17 13:15:46 +0200

    chore(root): remove .editorconfig; update package.json files to include engine specifications and enhance linting scripts across all packages

--- lint-staged.config.ts
+++ lint-staged.config.ts
@@ -2,22 +2,23 @@ import type { Configuration } from "lint-staged";
 
 const config: Configuration = {
 	"*": [
-		() => "concurrently 'npm run lint:clean:check' 'npm run lint:fs:check'",
+		(): string =>
+			"concurrently 'npm run lint:clean:check' 'npm run lint:fs:check' 'npm run lint:check' 'npm run lint:format:check'",
 	],
 	"apps/web/src/**/*.{ts,tsx}": [
-		() => "npm run lint:types:check -w apps/web",
-		() => "npm run lint:check",
-		() => "npm run lint:format:check",
+		(): string => "npm run lint:types:check -w apps/web",
+		(): string => "npm run lint:check -w apps/web",
+		(): string => "npm run lint:format:check -w apps/web",
 	],
 	"apps/backend/src/**/*.ts": [
-		() => "npm run lint:types:check -w apps/backend",
-		() => "npm run lint:check",
-		() => "npm run lint:format:check",
+		(): string => "npm run lint:types:check -w apps/backend",
+		(): string => "npm run lint:check -w apps/backend",
+		(): string => "npm run lint:format:check -w apps/backend",
 	],
 	"libs/shared/src/**/*.ts": [
-		() => "npm run lint:types:check -w libs/shared",
-		() => "npm run lint:check",
-		() => "npm run lint:format:check",
+		(): string => "npm run lint:types:check -w libs/shared",
+		(): string => "npm run lint:check -w libs/shared",
+		(): string => "npm run lint:format:check -w libs/shared",
 	],
 };
 
